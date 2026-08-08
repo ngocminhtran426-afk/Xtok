57e48daf995e809e4bbe86fe2f343abbef52e729
Your Name <you@example.com>
2026-08-08 23:11:23 +0700

    Fix API URL for production

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 import './index.css'
 import axios from 'axios'
 
-axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:4000'
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || ''
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
